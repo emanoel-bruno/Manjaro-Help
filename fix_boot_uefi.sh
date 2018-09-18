5cfac01393f70104bc2f8e875367a14f7ea02dc8
Emanoel Bruno <40868874+emanoel-bruno@users.noreply.github.com>
2018-09-18 04:15:09 -0300

    Update fix_boot_uefi.sh

--- fix_boot_uefi.sh
+++ fix_boot_uefi.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-#sudo grub-install
-#sudo cp /boot/grub/x86_64-efi/core.efi /boot/efi/EFI/boot/bootx64.efi
-#sudo efibootmgr -c -d /dev/sda -p 1 -L "manjaro" -l "\EFI\Manjaro\grubx64.efi"
-sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=manjaro --recheck
+sudo grub-install
+sudo cp /boot/grub/x86_64-efi/core.efi /boot/efi/EFI/boot/bootx64.efi
+sudo efibootmgr -c -d /dev/sda -p 1 -L "manjaro" -l "\EFI\Manjaro\grubx64.efi"
+#sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=manjaro --recheck
 sudo update-grub
