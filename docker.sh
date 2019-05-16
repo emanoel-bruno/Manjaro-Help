db8a749eb8b3d3071284a48d5f7a973211df0a50
Emanoel Bruno <40868874+emanoel-bruno@users.noreply.github.com>
2019-05-16 14:15:32 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Syyu
-sudo pacman -S docker
+sudo pacman -S docker docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
