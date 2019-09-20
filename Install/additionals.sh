74c0f12ea119615291ec862102166b3b8c7f0a64
Emanoel Bruno do Nascimento Guimarães <emanoelbruno22@outlook.com>
2019-09-20 02:31:55 -0300

    Improve content

---
+++ Install/additionals.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Java 12 and set as default
+sudo pacman -Syu jdk-openjdk
+sudo archlinux-java set java-12-openjdk
+
+# Install Yay(AUR)
+sudo pacman -Syu yay
+
+# Install Visual Studio Code
+sudo pacman -Syu code
+
+# Install Google 
+yay -Syua google-chrome
+
+# Install SMPlayer with additional content
+sudo pacman -Syu smplayer smplayer-skins smplayer-themes
+
+# Install JDownloader2
+yay -Syu jdownloader2
