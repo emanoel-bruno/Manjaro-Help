fa6fbd365178486f1aabe8e9838957cbf83ef06a
Emanoel Bruno do Nascimento Guimarães <emanoelbruno22@outlook.com>
2019-10-23 01:49:14 -0300

    Added Google Translator API to additionals.sh

--- Install/additionals.sh
+++ Install/additionals.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
+# Update pacman
+sudo pacman -Syyu --noconfirm
+
 # Install Java 12 and set as default
-sudo pacman -Syu jdk-openjdk
+sudo pacman -S --noconfirm jdk-openjdk
 sudo archlinux-java set java-12-openjdk
 
 # Install Yay(AUR)
-sudo pacman -Syu yay
+sudo pacman -S --noconfirm yay
 
 # Install Visual Studio Code
-sudo pacman -Syu code
+sudo pacman -S --noconfirm code
 
 # Install Google 
-yay -Syua google-chrome
+yay -S --noconfirma google-chrome
 
 # Install SMPlayer with additional content
-sudo pacman -Syu smplayer smplayer-skins smplayer-themes
+sudo pacman -S --noconfirm smplayer smplayer-skins smplayer-themes
 
 # Install JDownloader2
-yay -Syu jdownloader2
+yay -S --noconfirm jdownloader2
 
 # Install LaTeX
-sudo pacman -Ss texlive-most texlive-lang
+sudo pacman -S --noconfirm texlive-most texlive-lang
+
+# Scilab
+yay -S --noconfirm scilab-bin
+
+# Google Translator API for terminal
+sudo pacman -S --noconfirm translate-shell
